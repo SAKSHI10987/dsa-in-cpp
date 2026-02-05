b6c1eeb78a59f099871990a05924e6db65cdcefe
SAKSHI10987 <ssakshi.g22@gmail.com>
2026-02-05 19:56:18 +0530

    check size of char

--- arr.cpp
+++ arr.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
     int arr[5]={8,4};
     for(int i=0;i<5;i++){
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
     }
+    cout<<endl;
 
-    return 0;
+    char c='a';
+    cout<<sizeof(c);
 }
